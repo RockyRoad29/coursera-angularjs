b5eeb9325f99d4463c6a9e186e4a757ea7ca51cb
Michelle Baert <rocky.road@rocky-shore.net>
2016-10-27 21:59:42 +0200

    moving shopping list to pug template env and added a button

---
+++ assignments/module2/app.js
@@ -0,0 +1,11 @@
+{ "to_buy" : [
+       {"quantity": "1 bag", "name": "cookies"},
+       {"quantity": "1 kg", "name": "wheat flour"},
+       {"quantity": "2 packs", "name": "cooking chocolate"},
+       {"quantity": "1 bag", "name": "sultanas"},
+       {"quantity": "1 pound", "name": "pears"},
+       {"quantity": "1 box", "name": "vanilla icecream"},
+       {"quantity": "6 pc", "name": "fresh eggs"},
+       {"quantity": "200 ml", "name": "fresh cream"},
+       {"quantity": "1 pack", "name": "backing powder"}
+]}
